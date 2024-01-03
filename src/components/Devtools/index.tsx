3b40cd72741e0ef09899bbc93f88a446a6533b1c
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-01-03 09:58:37 +0100

    add devtools component

--- src/components/Devtools/index.tsx
+++ src/components/Devtools/index.tsx
@@ -1,13 +1,18 @@
 import {Pressable, Text} from 'react-native';
 import Performance from 'react-native-performance';
 
-const onPress = () => {
+const onPress = async () => {
     console.log('Devtools collecting performance metrics...');
-    fetch('http://localhost:8080', {
-        method: 'POST',
-        headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-        body: JSON.stringify(Performance.getEntries()),
-    });
+    // TODO: switch for your local IP
+    try {
+        await fetch('http://192.168.0.106:8080', {
+            method: 'POST',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify(Performance.getEntries()),
+        });
+    } catch (e) {
+        console.log('Error', e);
+    }
 };
 
 function Devtools() {

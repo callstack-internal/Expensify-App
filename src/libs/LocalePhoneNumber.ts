761503490648a74bd31c6552c98b506fbd68c02d
Github <olimpia.zurek@gmail.com>
2024-07-22 19:19:10 +0200

    cache formatPhoneNumber

--- src/libs/LocalePhoneNumber.ts
+++ src/libs/LocalePhoneNumber.ts
@@ -10,6 +10,8 @@ Onyx.connect({
     callback: (val) => (countryCodeByIP = val ?? 1),
 });
 
+const phoneNumberCache: Record<string, string> = {};
+
 /**
  * Returns a locally converted phone number for numbers from the same region
  * and an internationally converted phone number with the country code for numbers from other regions
@@ -19,33 +21,36 @@ function formatPhoneNumber(number: string): string {
         return '';
     }
 
+    if (phoneNumberCache[number]) {
+        return phoneNumberCache[number];
+    }
+
     // eslint-disable-next-line no-param-reassign
     number = number.replace(/ /g, '\u00A0');
 
     // do not parse the string, if it doesn't contain the SMS domain and it's not a phone number
     if (number.indexOf(CONST.SMS.DOMAIN) === -1 && !CONST.REGEX.DIGITS_AND_PLUS.test(number)) {
+        phoneNumberCache[number] = number;  // Cache the unmodified number
         return number;
     }
+
     const numberWithoutSMSDomain = Str.removeSMSDomain(number);
     const parsedPhoneNumber = parsePhoneNumber(numberWithoutSMSDomain);
 
-    // return the string untouched if it's not a phone number
+    let formattedNumber: string;
     if (!parsedPhoneNumber.valid) {
-        if (parsedPhoneNumber.number?.international) {
-            return parsedPhoneNumber.number.international;
-        }
-        return numberWithoutSMSDomain;
+        formattedNumber = parsedPhoneNumber.number?.international ?? numberWithoutSMSDomain;
+    } else {
+        const regionCode = parsedPhoneNumber.countryCode;
+        formattedNumber = regionCode === countryCodeByIP
+            ? parsedPhoneNumber.number.national
+            : parsedPhoneNumber.number.international;
     }
 
-    const regionCode = parsedPhoneNumber.countryCode;
-
-    if (regionCode === countryCodeByIP) {
-        return parsedPhoneNumber.number.national;
-    }
-
-    return parsedPhoneNumber.number.international;
+    // Cache the formatted number
+    phoneNumberCache[number] = formattedNumber;
+    return formattedNumber;
 }
-
 export {
     // eslint-disable-next-line import/prefer-default-export
     formatPhoneNumber,

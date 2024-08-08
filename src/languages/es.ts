de535bc93224b22b5527ea4928c393c44eb89cec
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-08-08 02:37:36 +0100

    fix: apply validations to json and onyx data properties

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -4865,6 +4865,7 @@ export default {
         preview: 'Previa:',
         missingProperty: ({propertyName}) => `Falta ${propertyName}`,
         invalidProperty: ({propertyName, expectedType}) => `Propiedad inválida: ${propertyName} - Esperado: ${expectedType}`,
+        invalidValue: ({expectedValues}) => `Valor inválido - Esperado: ${expectedValues}`,
         createReportAction: 'Crear Report Action',
         reportAction: 'Report Action',
         report: 'Report',

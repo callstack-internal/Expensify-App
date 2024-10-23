a60eea9aa761d557387fc73bff412dd7c3d63ed1
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-10-23 15:37:21 +0200

    report action item message

--- src/pages/home/report/ReportActionItemList/ReportActionItemContent.tsx
+++ src/pages/home/report/ReportActionItemList/ReportActionItemContent.tsx
@@ -10,53 +10,37 @@ import type {ActionableItem} from '@components/ReportActionItem/ActionableItemBu
 import {ShowContextMenuContext} from '@components/ShowContextMenuContext';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
+import usePermissions from '@hooks/usePermissions';
 import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
 import * as ReportActionsUtils from '@libs/ReportActionsUtils';
 import * as ReportUtils from '@libs/ReportUtils';
+import shouldRenderAddPaymentCard from '@libs/shouldRenderAppPaymentCard';
+import * as TransactionUtils from '@libs/TransactionUtils';
 import ReportActionItemMessage from '@pages/home/report/ReportActionItemMessage';
 import ReportActionItemMessageEdit from '@pages/home/report/ReportActionItemMessageEdit';
+import * as Member from '@userActions/Policy/Member';
+import * as Report from '@userActions/Report';
 import * as User from '@userActions/User';
 import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import ROUTES from '@src/ROUTES';
 import type * as OnyxTypes from '@src/types/onyx';
+import type {JoinWorkspaceResolution} from '@src/types/onyx/OriginalMessage';
 
 type Props = {
-    mentionReportContextValue: {currentReportID: string};
-    contextValue: {
-        anchor: any;
-        report: OnyxTypes.Report;
-        action: OnyxTypes.ReportAction;
-        checkIfContextMenuActive: () => void;
-    };
-    attachmentContextValue: {reportID: string; type: string};
-    draftMessage: string | undefined;
     reportID: string;
     action: OnyxTypes.ReportAction;
     displayAsGroup: boolean;
     isHidden: boolean;
     hasBeenFlagged: boolean;
     updateHiddenState: (isHidden: boolean) => void;
-    actionableItemButtons: ActionableItem[];
     report: OnyxTypes.Report;
-    blockedFromConcierge: boolean;
-    reportNameValuePairs: Record<string, any>;
     index: number;
     textInputRef: React.RefObject<any>;
 };
 
-function ReportActionItemContent({
-    // draftMessage,
-    reportID,
-    action,
-    displayAsGroup,
-    isHidden,
-    hasBeenFlagged,
-    updateHiddenState,
-    actionableItemButtons,
-    report,
-    reportNameValuePairs,
-    index,
-    textInputRef,
-}: Props) {
+function ReportActionItemContent({reportID, action, displayAsGroup, isHidden, hasBeenFlagged, updateHiddenState, report, index, textInputRef}: Props) {
     const originalReportID = useMemo(() => ReportUtils.getOriginalReportID(reportID, action) ?? '-1', [reportID, action]);
     const [draftMessage] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS_DRAFTS}${originalReportID}`, {
         selector: (draftMessagesForReport) => {
@@ -64,9 +48,11 @@ function ReportActionItemContent({
             return typeof matchingDraftMessage === 'string' ? matchingDraftMessage : matchingDraftMessage?.message;
         },
     });
-    console.log('draftMessage', draftMessage);
     const styles = useThemeStyles();
     const {translate} = useLocalize();
+    const isActionableWhisper =
+        ReportActionsUtils.isActionableMentionWhisper(action) || ReportActionsUtils.isActionableTrackExpense(action) || ReportActionsUtils.isActionableReportMentionWhisper(action);
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report?.reportID || '-1'}`);
     const blockedFromConcierge = useBlockedFromConcierge();
     const mentionReportContextValue = useMemo(() => ({currentReportID: report?.reportID ?? '-1'}), [report?.reportID]);
     const attachmentContextValue = useMemo(() => ({reportID, type: CONST.ATTACHMENT_TYPE.REPORT}), [reportID]);
@@ -80,8 +66,117 @@ function ReportActionItemContent({
             // checkIfContextMenuActive: toggleContextMenuFromActiveReportAction,
             isDisabled: false,
         }),
-        [report, action, reportNameValuePairs],
+        [report, reportNameValuePairs, action],
     );
+    const {canUseP2PDistanceRequests} = usePermissions();
+
+    const actionableItemButtons: ActionableItem[] = useMemo(() => {
+        if (ReportActionsUtils.isActionableAddPaymentCard(action) && shouldRenderAddPaymentCard()) {
+            return [
+                {
+                    text: 'subscription.cardSection.addCardButton',
+                    key: `${action.reportActionID}-actionableAddPaymentCard-submit`,
+                    onPress: () => {
+                        Navigation.navigate(ROUTES.SETTINGS_SUBSCRIPTION_ADD_PAYMENT_CARD);
+                    },
+                    isMediumSized: true,
+                    isPrimary: true,
+                },
+            ];
+        }
+
+        if (!isActionableWhisper && (!ReportActionsUtils.isActionableJoinRequest(action) || ReportActionsUtils.getOriginalMessage(action)?.choice !== ('' as JoinWorkspaceResolution))) {
+            return [];
+        }
+
+        if (ReportActionsUtils.isActionableTrackExpense(action)) {
+            const transactionID = ReportActionsUtils.getOriginalMessage(action)?.transactionID;
+            return [
+                ...(!TransactionUtils.isDistanceRequest(TransactionUtils.getTransaction(transactionID ?? '-1')) || canUseP2PDistanceRequests
+                    ? [
+                          {
+                              text: 'actionableMentionTrackExpense.submit',
+                              key: `${action.reportActionID}-actionableMentionTrackExpense-submit`,
+                              onPress: () => {
+                                  ReportUtils.createDraftTransactionAndNavigateToParticipantSelector(transactionID ?? '0', reportID, CONST.IOU.ACTION.SUBMIT, action.reportActionID);
+                              },
+                              isMediumSized: true,
+                          } as ActionableItem,
+                      ]
+                    : []),
+                {
+                    text: 'actionableMentionTrackExpense.categorize',
+                    key: `${action.reportActionID}-actionableMentionTrackExpense-categorize`,
+                    onPress: () => {
+                        ReportUtils.createDraftTransactionAndNavigateToParticipantSelector(transactionID ?? '0', reportID, CONST.IOU.ACTION.CATEGORIZE, action.reportActionID);
+                    },
+                    isMediumSized: true,
+                },
+                {
+                    text: 'actionableMentionTrackExpense.share',
+                    key: `${action.reportActionID}-actionableMentionTrackExpense-share`,
+                    onPress: () => {
+                        ReportUtils.createDraftTransactionAndNavigateToParticipantSelector(transactionID ?? '0', reportID, CONST.IOU.ACTION.SHARE, action.reportActionID);
+                    },
+                    isMediumSized: true,
+                },
+                {
+                    text: 'actionableMentionTrackExpense.nothing',
+                    key: `${action.reportActionID}-actionableMentionTrackExpense-nothing`,
+                    onPress: () => {
+                        Report.dismissTrackExpenseActionableWhisper(reportID, action);
+                    },
+                    isMediumSized: true,
+                },
+            ];
+        }
+
+        if (ReportActionsUtils.isActionableJoinRequest(action)) {
+            return [
+                {
+                    text: 'actionableMentionJoinWorkspaceOptions.accept',
+                    key: `${action.reportActionID}-actionableMentionJoinWorkspace-${CONST.REPORT.ACTIONABLE_MENTION_JOIN_WORKSPACE_RESOLUTION.ACCEPT}`,
+                    onPress: () => Member.acceptJoinRequest(reportID, action),
+                    isPrimary: true,
+                },
+                {
+                    text: 'actionableMentionJoinWorkspaceOptions.decline',
+                    key: `${action.reportActionID}-actionableMentionJoinWorkspace-${CONST.REPORT.ACTIONABLE_MENTION_JOIN_WORKSPACE_RESOLUTION.DECLINE}`,
+                    onPress: () => Member.declineJoinRequest(reportID, action),
+                },
+            ];
+        }
+
+        if (ReportActionsUtils.isActionableReportMentionWhisper(action)) {
+            return [
+                {
+                    text: 'common.yes',
+                    key: `${action.reportActionID}-actionableReportMentionWhisper-${CONST.REPORT.ACTIONABLE_REPORT_MENTION_WHISPER_RESOLUTION.CREATE}`,
+                    onPress: () => Report.resolveActionableReportMentionWhisper(reportID, action, CONST.REPORT.ACTIONABLE_REPORT_MENTION_WHISPER_RESOLUTION.CREATE),
+                    isPrimary: true,
+                },
+                {
+                    text: 'common.no',
+                    key: `${action.reportActionID}-actionableReportMentionWhisper-${CONST.REPORT.ACTIONABLE_REPORT_MENTION_WHISPER_RESOLUTION.NOTHING}`,
+                    onPress: () => Report.resolveActionableReportMentionWhisper(reportID, action, CONST.REPORT.ACTIONABLE_REPORT_MENTION_WHISPER_RESOLUTION.NOTHING),
+                },
+            ];
+        }
+
+        return [
+            {
+                text: 'actionableMentionWhisperOptions.invite',
+                key: `${action.reportActionID}-actionableMentionWhisper-${CONST.REPORT.ACTIONABLE_MENTION_WHISPER_RESOLUTION.INVITE}`,
+                onPress: () => Report.resolveActionableMentionWhisper(reportID, action, CONST.REPORT.ACTIONABLE_MENTION_WHISPER_RESOLUTION.INVITE),
+                isPrimary: true,
+            },
+            {
+                text: 'actionableMentionWhisperOptions.nothing',
+                key: `${action.reportActionID}-actionableMentionWhisper-${CONST.REPORT.ACTIONABLE_MENTION_WHISPER_RESOLUTION.NOTHING}`,
+                onPress: () => Report.resolveActionableMentionWhisper(reportID, action, CONST.REPORT.ACTIONABLE_MENTION_WHISPER_RESOLUTION.NOTHING),
+            },
+        ];
+    }, [action, isActionableWhisper, canUseP2PDistanceRequests, reportID]);
 
     return (
         <MentionReportContext.Provider value={mentionReportContextValue}>

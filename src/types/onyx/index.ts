ac32b660b24093a5c86e2d962feb366eb67c4332
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-01-12 14:35:26 +0100

    add PERSONAL_AND_WORKSPACE_CARD_LIST derived value

--- src/types/onyx/index.ts
+++ src/types/onyx/index.ts
@@ -28,7 +28,12 @@ import type Credentials from './Credentials';
 import type Currency from './Currency';
 import type {CurrencyList} from './Currency';
 import type CustomStatusDraft from './CustomStatusDraft';
-import type {OutstandingReportsByPolicyIDDerivedValue, ReportAttributesDerivedValue, ReportTransactionsAndViolationsDerivedValue} from './DerivedValues';
+import type {
+    OutstandingReportsByPolicyIDDerivedValue,
+    PersonalAndWorkspaceCardListDerivedValue,
+    ReportAttributesDerivedValue,
+    ReportTransactionsAndViolationsDerivedValue,
+} from './DerivedValues';
 import type DismissedProductTraining from './DismissedProductTraining';
 import type DismissedReferralBanners from './DismissedReferralBanners';
 import type Domain from './Domain';
@@ -305,6 +310,7 @@ export type {
     LastSearchParams,
     ReportTransactionsAndViolationsDerivedValue,
     OutstandingReportsByPolicyIDDerivedValue,
+    PersonalAndWorkspaceCardListDerivedValue,
     ScheduleCallDraft,
     ValidateUserAndGetAccessiblePolicies,
     VacationDelegate,

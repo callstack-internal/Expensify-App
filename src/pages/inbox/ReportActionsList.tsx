9b03b34f67577b090887dc50a47798585e44ec2c
Hubert Sosinski <hubert.sosinski@callstack.com>
2026-04-17 11:19:20 +0200

    stylistic fixes

--- src/pages/inbox/ReportActionsList.tsx
+++ src/pages/inbox/ReportActionsList.tsx
@@ -1,6 +1,5 @@
 import {useRoute} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
 import MoneyRequestReportActionsList from '@components/MoneyRequestReportView/MoneyRequestReportActionsList';
 import ReportActionsSkeletonView from '@components/ReportActionsSkeletonView';
 import useNetwork from '@hooks/useNetwork';

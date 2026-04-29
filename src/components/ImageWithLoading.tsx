8c52e88e25b6e2d0440d4d717330a611e83dfea3
Hubert Sosinski <hubert.sosinski@callstack.com>
2026-04-29 13:38:26 +0200

    fix issue on mobile

--- src/components/ImageWithLoading.tsx
+++ src/components/ImageWithLoading.tsx
@@ -88,7 +88,7 @@ function ImageWithLoading({
             style={[styles.w100, styles.h100, containerStyles]}
             onLayout={onLayout}
         >
-            {isLoading && !!previewUri && !isImageCached && (
+            {isLoading && !!previewUri && (
                 // eslint-disable-next-line react-native-a11y/has-valid-accessibility-ignores-invert-colors -- Custom Image wrapper does not support this prop.
                 <Image
                     // eslint-disable-next-line react/jsx-props-no-spreading
